e45a931ffdc69e35d6ed12794b3028c499aaeade
raushnn <raushan026@gmail.com>
2021-10-10 20:39:05 +0530

    added dataset

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #here we'll write the preprocessed code
+import pandas as pd
+import numpy  as  np
+from nltk.tokenize import sent_tokenize, word_tokenize
+import nltk
+import re
+# from sklearn.datasets import fetch_20newsgroups
+df= pd.read_csv("data/IMDB Dataset.csv")
+print(df[:5])
+
